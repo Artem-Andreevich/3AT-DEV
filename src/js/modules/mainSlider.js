1d2ef67aad924496cee654ac9c167607978304e2
ARTEM ANDREEVICH <ops210@mail.ru>
2024-09-19 11:26:15 +0500

    add slider feedback

--- src/js/modules/mainSlider.js
+++ src/js/modules/mainSlider.js
@@ -1,16 +1,54 @@
 function mainSlider(){
-	const slider = document.querySelector('.main-slider__items')
+	const slider = document.querySelector('.feedback-slider__items')
 	$(slider)?.slick({
 		infinite: true,
 		speed: 200,
-		slidesToShow: 1,
-		dots: true,
-		appendDots: '.main-slider__dots',
-		arrows: false,
+		slidesToShow: 3,
+		arrows: true,
+		appendDots: '.feedback-slider__dots',
+		dots: false,
+		prevArrow: '.slider-prev',
+		nextArrow: '.slider-next',
 		waitForAnimate: true,
 		focusOnSelect: true,
-		fade: true,
+		fade: false,
 		LazyLoad: 'ondemand',
+		responsive: [
+			{
+				breakpoint: 1340,
+				settings: {
+					centerMode: true,
+					centerPadding: '180px',
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 991,
+				settings: {
+					centerMode: true,
+					centerPadding: '120px',
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 767,
+				settings: {
+					centerMode: true,
+					centerPadding: '120px',
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 576,
+				settings: {
+					arrows: false,
+					dots: true,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 1
+				}
+			},
+		]
 	});
 }
 
